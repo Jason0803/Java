380d1952aedb6f73b49a658ec2ad12829db121ab
Jason <c3171700@uon.edu.au>
2015-05-07 17:32:12 +0900

    Reduced Large Amount of Code
    Reduce and/or deleted some codes - that are weird, messy or not used

--- isis/2.TagHighlighter/src/ReadAndHighlightTag.java
+++ isis/2.TagHighlighter/src/ReadAndHighlightTag.java
@@ -17,13 +17,6 @@ import javax.swing.text.StyleContext;
 import javax.swing.text.StyledDocument;
 
 
-					/* 	LAST UPDATED: 06/05/2015 4:34PM 	*/
-					/* 		Algorithm Updated 		*/
-					/*		 Fastened Work			*/
-					/*	      All Found Errors Gone		*/
-					/*	Errors gone, just need extenstion	*/
-
-
 /*
  * 
  *	This application is made for following purposes:
@@ -34,10 +27,11 @@ import javax.swing.text.StyledDocument;
  *
  */
 //--------------------------------------------------------------------------------------------------------------------//
+
 @SuppressWarnings("serial")
-public class ReadAndHighlightTag extends JPanel
-                             implements ActionListener 
+public class ReadAndHighlightTag extends JPanel implements ActionListener 
 {
+
 	ArrayList<String> entire = new ArrayList<String>();
 	ArrayList<Segment> segments = new ArrayList<Segment>();
     JButton openButton;
@@ -49,7 +43,6 @@ public class ReadAndHighlightTag extends JPanel
     private JTextField txtTagtag;
     private static File file;
     private String tag;
-    private String anotherTag;						// For extension: 'Searching 2 tags'
     private static final String seg = "<Segment>";
 	private static final String src = "<Source>";
 	private static final String  trg = "<Target>";
@@ -57,8 +50,6 @@ public class ReadAndHighlightTag extends JPanel
 	int segmentCount = 0;
     BufferedReader br;
     private final JScrollPane scrollPane = new JScrollPane();
-	private boolean CHECK;
-
 
 // -------------------------------------------------------------------------------------------------------------------//
     public ReadAndHighlightTag()  // Constructor
@@ -125,12 +116,10 @@ public class ReadAndHighlightTag extends JPanel
 		panel.add(scrollPane);
 		
 		txtpnabc = new JEditorPane();
+		txtpnabc.setBackground(SystemColor.control);
 		txtpnabc.setContentType("text/html");
 		scrollPane.setViewportView(txtpnabc);
-		//txtpnabc.setText("<html><font color='red'><b></font>\uC77C\uBC18 \uAC80\uC0C9<font color='red'></b></font></html>");
-        // to check whether HTML Source works
-		
-		
+				
 		JLabel lblResult = new JLabel("Result :");
 		lblResult.setBounds(22, 109, 57, 15);
 	    add(lblResult);        
@@ -147,8 +136,8 @@ public class ReadAndHighlightTag extends JPanel
 
             if (returnVal == JFileChooser.APPROVE_OPTION) 
             {
-                file = fc.getSelectedFile();
-                if( file.getName().indexOf(".EXP") == -1 )
+            	file = fc.getSelectedFile();
+            	if( file.getName().indexOf(".EXP") == -1 )
                 {
                     JOptionPane.showMessageDialog(scrollPane, "You should choose .EXP File !", "Warning",
                             JOptionPane.WARNING_MESSAGE);
@@ -163,21 +152,19 @@ public class ReadAndHighlightTag extends JPanel
             		br = new BufferedReader(new InputStreamReader
             				(new FileInputStream(file.getPath()), Charset.forName("UTF-16")));
             		 
-                		String line = br.readLine();
-                		while( line != null )
-                		{
-                			entire.add(line);
-                			line = br.readLine();
-                		}
-                		findSegment(entire);
-                		
-                		br.close();
+                	String line = br.readLine();
+                	while( line != null )
+                	{
+                		entire.add(line);
+                		line = br.readLine();
+                	}
+                	
+                	findSegment(entire);
+                	br.close();
                 }
                 catch(Exception error) { error.printStackTrace(); } 
             } 
-            else 
-            {
-            	
+            else{
             	textField.setText("File Choose Cancelled by user");
             }
         }
@@ -185,6 +172,8 @@ public class ReadAndHighlightTag extends JPanel
         {
         	try
         	{
+        		
+        		
         		int i = 0;
         		StringBuilder contents = new StringBuilder();
         		txtpnabc.setText("");
@@ -193,24 +182,7 @@ public class ReadAndHighlightTag extends JPanel
         		
         		while(i < segments.size() )
         		{
-        			if( (segments.get(i).getSource().indexOf(_tag+">")!= -1||segments.get(i).getSource().indexOf(_tag+" ")!=-1) && segments.get(i).getTarget().indexOf(_tag) == -1)
-        			{	
-        				// tag in source, but not in target
-        				contents.append("Segment: " + segments.get(i).getSegment() + "\n");
-        				contents.append( "Source: " + segments.get(i).getSource() + "\n");
-        				contents.append( "Target: " + segments.get(i).getTarget() + "\n\n");
-        				
-        				
-        			}
-        			else if( (segments.get(i).getTarget().indexOf(_tag+">")!= -1 || segments.get(i).getTarget().indexOf(_tag+" ")!= -1) && segments.get(i).getSource().indexOf(_tag) == -1)
-        			{
-        				// tag in target, but not in source
-        				contents.append("Segment: " +  segments.get(i).getSegment() + "\n");
-        				contents.append(" Source: " + segments.get(i).getSource() + "\n");
-        				contents.append(" Target: " + segments.get(i).getTarget() + "\n\n");
-        				
-        			}
-        			else if( (segments.get(i).getSource().indexOf(_tag+">")!= -1 || segments.get(i).getSource().indexOf(_tag+" ")!=-1) &&
+        			if( (segments.get(i).getSource().indexOf(_tag+">")!= -1 || segments.get(i).getSource().indexOf(_tag+" ")!=-1) ||
         			( segments.get(i).getTarget().indexOf(_tag+">")!= -1 || segments.get(i).getTarget().indexOf(_tag+" ")!= -1) )
         			{
         				// tag in both source and target
@@ -285,65 +257,6 @@ public class ReadAndHighlightTag extends JPanel
 	} 
 	catch (BadLocationException ex) { ex.printStackTrace(); }
 }
-//--------------------------------------------------------------------------------------------------------------------//
-    void appendString(String str) throws Exception
-    {
-        StyledDocument document = (StyledDocument) txtpnabc.getDocument();
-
- 
-        document.insertString(document.getLength(), str, null);
-     }
-// -------------------------------------------------------------------------------------------------------------------//
-    public String tagColouring( String line, String tag ) 
-    {
-    	// This method is being used without 'tag' exists in the 'line'
-    	// as this method will be used where already checked for that case
-    	
-    	StringBuilder sb = new StringBuilder();
-    	String temp;
-    	String result;
-    	if( line.indexOf(tag) == 0 )																			// 1. In case the tag located at the beginning of the line
-    	{	
-    		sb.append("<html><font color='red'>");
-    		temp = line.substring(0, line.indexOf(">")+1);														// Colouring opening tag
-    		sb.append(temp + "</font></html>");
-    		
-    		temp = line.substring( line.indexOf(">")+1, line.indexOf( "</"+ tag ) );							// Content_1
-    		sb.append(temp);
-    		
-    		temp = line.substring( line.indexOf( "</" + tag), line.indexOf(">", line.indexOf("</" + tag))+1 );	// Colouring closing tag
-    		sb.append("<html><font color='red'>");
-    		sb.append(temp);
-    		sb.append("</font></html>");
-    		
-    		temp = line.substring( line.indexOf(">", line.indexOf("</" + tag)) );								// Content_2
-    		sb.append(temp); // this could be just "" in case there is no more content in current line
-    	}
-    	else																									// 2. In case the tag located between contents of the line
-    	{		
-    		temp = line.substring(0, line.indexOf("<"+tag));													// Content_1				
-    		sb.append(temp);
-    		
-    		temp = line.substring( line.indexOf("<"+tag), line.indexOf(">", line.indexOf(tag))+1 );				// Colouring opening tag
-    		sb.append("<html><font color='red'>");
-    		sb.append(temp);
-    		sb.append("</font></html>");
-    		
-    		temp = line.substring( line.indexOf(">", line.indexOf(tag))+1, line.indexOf("</" + tag) );			// Content_2
-    		sb.append(temp);
-    		
-    		temp = line.substring( line.indexOf("</" + tag), line.indexOf(">", line.indexOf("</"+tag))+1);		// Colouring closing tag
-    		sb.append("<html><font color='red'>");
-    		sb.append(temp);
-    		sb.append("</font></html>");
-    		
-    		temp = line.substring( line.indexOf(">", line.indexOf("</"+tag))+1 );									// Content_3
-    		sb.append(temp);
-    	}
-    	
-    	result = sb.toString();
-    	return result;
-    }
 // -------------------------------------------------------------------------------------------------------------------//   
     private static void createAndShowGUI() {
         //Create and set up the window.'
@@ -455,5 +368,5 @@ public class ReadAndHighlightTag extends JPanel
             }
         });
     }
-// -------------------------------------------------------------------------------------------------------------------//   
 }
+// -------------------------------------------------------------------------------------------------------------------//   
