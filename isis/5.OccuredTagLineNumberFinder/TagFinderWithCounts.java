e51fda39caf4b0d492dccd7e08405662847d13ee
Jason <c3171700@uon.edu.au>
2015-05-12 17:54:01 +0900

    Update TagFinderWithCounts.java

--- isis/5.OccuredTagLineNumberFinder/TagFinderWithCounts.java
+++ isis/5.OccuredTagLineNumberFinder/TagFinderWithCounts.java
@@ -1,5 +1,6 @@
 import java.awt.EventQueue;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -8,62 +9,51 @@ import javax.swing.JTextField;
 import javax.swing.JSpinner;
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.File;
 
 
-public class TagFinderWithCounts {
-
-	private JFrame frame;
+public class TagFinderWithCounts extends JPanel implements ActionListener{
+	private static JFrame frame;
+	private JPanel panel;
+	private JLabel lblFile;
+	private JLabel lblTagToFind;
+	private JLabel lblTagCount;
 	private JTextField textField;
 	private JTextField textField_1;
-	private JTextField lineNumberField;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TagFinderWithCounts window = new TagFinderWithCounts();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
+	private JButton btnBrowse;
+	private JSpinner spinner;
+	private JButton btnFindLineNumber;
+	private JScrollPane scrollPane;
+	private JEditorPane editorPane;
+	private JFileChooser fc;
+	private File file;
+	
 	public TagFinderWithCounts() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 564, 336);
+		frame.setBounds(100, 100, 456, 330);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 543, 173);
+		panel = new JPanel();
+		panel.setBounds(0, 0, 443, 173);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblFile = new JLabel("File : ");
+		lblFile = new JLabel("File : ");
 		lblFile.setBounds(40, 35, 48, 20);
 		panel.add(lblFile);
 		
-		JLabel lblTagToFind = new JLabel("Tag to find : ");
+		lblTagToFind = new JLabel("Tag to find : ");
 		lblTagToFind.setBounds(40, 77, 80, 20);
 		panel.add(lblTagToFind);
 		
-		JLabel lblTagCount = new JLabel("Tag Occurence");
-		lblTagCount.setBounds(239, 80, 91, 17);
+		lblTagCount = new JLabel("Tag Occurence : ");
+		lblTagCount.setBounds(40, 127, 105, 18);
 		panel.add(lblTagCount);
 		
 		textField = new JTextField();
@@ -76,29 +66,69 @@ public class TagFinderWithCounts {
 		panel.add(textField_1);
 		textField_1.setColumns(10);
 		
-		JSpinner spinner = new JSpinner();
-		spinner.setBounds(342, 77, 62, 20);
+		spinner = new JSpinner();
+		spinner.setBounds(165, 125, 62, 20);
 		panel.add(spinner);
 		
-		JButton btnBrowse = new JButton("Browse..");
+		btnBrowse = new JButton("Browse..");
+
 		btnBrowse.setBounds(307, 34, 97, 23);
 		panel.add(btnBrowse);
 		
-		JButton btnFindLineNumber = new JButton("Find Line Number");
-		btnFindLineNumber.setBounds(40, 119, 151, 28);
+		btnFindLineNumber = new JButton("Search");
+		btnFindLineNumber.setBounds(253, 77, 151, 72);
 		panel.add(btnFindLineNumber);
 		
-		lineNumberField = new JTextField();
-		lineNumberField.setBounds(324, 121, 80, 26);
-		panel.add(lineNumberField);
-		lineNumberField.setColumns(10);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(0, 174, 543, 124);
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 174, 443, 118);
 		frame.getContentPane().add(scrollPane);
 		
-		JEditorPane editorPane = new JEditorPane();
+		editorPane = new JEditorPane();
 		editorPane.setContentType("text/html\r\n");
 		scrollPane.setViewportView(editorPane);
+		
+		fc = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(".dita Files", "dita");
+	    fc.addChoosableFileFilter(filter);
+	    
+		btnBrowse.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			if( e.getSource() == btnBrowse ){
+				int returnVal = fc.showOpenDialog( TagFinderWithCounts.this);
+	            if (returnVal == JFileChooser.APPROVE_OPTION){
+	            	file = fc.getSelectedFile();
+	                //This is where a real application would open the file.
+	                textField.setText(file.getName());   
+	            }
+	            }
+			}
+		});
+		
+		
 	}
+	private static void createAndShowGUI(){
+		        //Create and set up the window.'
+		        frame = new JFrame("TagFinder");
+		        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 
+		        //Add content to the window.
+		        frame.getContentPane().add(new TagFinderWithCounts());
+		 
+		        //Display the window.
+		        frame.pack();
+		        frame.setVisible(true);
+		 }
+	public static void main(String[] args) {
+	EventQueue.invokeLater(new Runnable() {
+		public void run() {
+			try {
+				UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+                createAndShowGUI();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	});
+}
+
 }
