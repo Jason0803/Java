02c9d5461281457e88072b5770bfc75d3e0371aa
Jason <c3171700@uon.edu.au>
2017-08-24 18:52:00 +0900

    Collections

--- workspace/java23_Collection/src/props/test/HashSetTest.java
+++ workspace/java23_Collection/src/props/test/HashSetTest.java
@@ -1,10 +1,27 @@
 package props.test;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class HashSetTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Set<String> set = new HashSet<String>();
+		set.add("IU");
+		set.add("Jason");
+		set.add("Sunny");
+		
+		System.out.println(set);
+		
+		System.out.println(set.contains("IU"));
+		
+		System.out.println(set);
+		
+		Iterator<String> order = set.iterator();
+		while(order.hasNext()){
+			System.out.println(order.next());
+		}
 	}
 
 }
