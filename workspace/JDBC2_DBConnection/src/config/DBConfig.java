1a0088b2af8d1a6d8c9bb4b9bb9a4c634a73b454
Jason <c3171700@uon.edu.au>
2017-09-04 11:03:19 +0900

    JDBCTest2_1

--- workspace/JDBC2_DBConnection/src/config/DBConfig.java
+++ workspace/JDBC2_DBConnection/src/config/DBConfig.java
@@ -2,7 +2,7 @@ package config;
 
 public interface DBConfig {
 	String DRIVER = "oracle.jdbc.driver.OracleDriver";
-	String URL = "jdbc:oracle:thin:@192.168.30.131:1521:xe";
+	String URL = "jdbc:oracle:thin:@192.168.30.129:1521:xe";
 	String USER = "hr";
 	String PASSWORD = "hr";
 }
