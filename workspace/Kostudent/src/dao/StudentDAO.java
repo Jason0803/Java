ac5aaa102cdf0e03f02632305782730839d42253
Jason <c3171700@uon.edu.au>
2017-09-22 10:14:45 +0900

    KOStudent_Login_Fix (break)

--- workspace/Kostudent/src/dao/StudentDAO.java
+++ workspace/Kostudent/src/dao/StudentDAO.java
@@ -7,6 +7,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import config.DBConfig;
 import jdbc.exception.DuplicateIdException;
 import jdbc.exception.RecordNotFoundException;
@@ -17,9 +21,17 @@ public class StudentDAO {
 
 	// ---------------------------------- for singleton  ----------------------------------//
 	private static StudentDAO dao = new StudentDAO();
-	
+	private DataSource ds;
 	private StudentDAO() {
-
+		try {
+			InitialContext ic = new InitialContext();
+			ds = (DataSource)ic.lookup("java:comp/env/jdbc/oracleDB");
+			System.out.println("DataSource..LookUp Success !");
+			// DataSource ds = ic.lookup(name);
+		} catch (NamingException e) {
+			System.out.println("DataSource..LookUp Fail :( ");
+			e.printStackTrace();
+		}
 	}
 	public static StudentDAO getInstance() {
 		return dao;
@@ -29,9 +41,10 @@ public class StudentDAO {
 	public Connection connection() {
 		Connection conn = null;
 		try {
-			conn = 
-				DriverManager.getConnection(DBConfig.URL, DBConfig.USER, DBConfig.PASSWORD);
+			conn = ds.getConnection();
+				//DriverManager.getConnection(DBConfig.URL, DBConfig.USER, DBConfig.PASSWORD);
 				// System.out.println("DB Connnected!");
+			System.out.println("Hello, DB Connected ");
 		} catch (SQLException e) {
 			System.out.println("DB Disconnected !");
 		}
@@ -49,65 +62,62 @@ public class StudentDAO {
 	}
 
 	// ---------------------------------- for SELECT  ----------------------------------//
-		public ArrayList<Student> getAllStudent() throws SQLException{
-			ArrayList<Student> result = null;
-			Connection conn = connection();
-			PreparedStatement ps = 
-					conn.prepareStatement("SELECT * FROM student");
+	public ArrayList<Student> getAllStudent() throws SQLException{
+		ArrayList<Student> result = null;
+		Connection conn = connection();
+		PreparedStatement ps = 
+				conn.prepareStatement("SELECT * FROM student");
 			
-			ResultSet rs = ps.executeQuery();
+		ResultSet rs = ps.executeQuery();
 			
-			while(rs.next()) {
-				result.add(new Student(rs.getInt("classno"), 
-										rs.getString("sid"), 
-										rs.getString("password")));
-			}
-				
-			return result;
+		while(rs.next()) {
+			result.add(new Student(rs.getInt("classno"), 
+									rs.getString("sid"), 
+									rs.getString("password")));
 		}
+				
+		return result;
+	}
 	
-		// ---------------------------------- for INSERT ---------------------------------- //
-		public void addMember(Student student) throws SQLException, DuplicateIdException {
-			Connection conn = null;
-			PreparedStatement ps = null;
-		
-			try {
+	// ---------------------------------- for INSERT ---------------------------------- //
+	public void addMember(Student student) throws SQLException, DuplicateIdException {
+		Connection conn = null;
+		PreparedStatement ps = null;
+	
+		try {
+			conn = connection();
+				
+			if( !doesExist(student.getName(), conn) ){
 				conn = connection();
-				
-				if( !doesExist(student.getName(), conn) ){
-					conn = connection();
-					System.out.println("connection");
+				System.out.println("connection");
 					
-					ps = conn.prepareStatement("INSERT INTO student VALUES(?,?,?)");
-					System.out.println("ps");
-					ps.setString(1, student.getName());
-					ps.setInt(2, student.getUserClass());
-					ps.setString(3, student.getPassword());
+				ps = conn.prepareStatement("INSERT INTO student VALUES(?,?,?)");
+				System.out.println("ps");
+				ps.setString(1, student.getName());
+				ps.setInt(2, student.getUserClass());
+				ps.setString(3, student.getPassword());
 					
-					int row = ps.executeUpdate();
-					System.out.println("Sucess ? :" + row);
-				} else  {
-					throw new DuplicateIdException("Already Existing ID !");
-				}
-			} finally {
-				
-				System.out.println("Adding student done");
-				try{
-					closeAll(ps, conn);
-				} catch(SQLException e) {
-					e.printStackTrace();
-				}
+				int row = ps.executeUpdate();
+				System.out.println("Sucess ? :" + row);
+			} else  {
+				throw new DuplicateIdException("Already Existing ID !");
 			}
-			
-		}
+		} finally {
+				
+			System.out.println("Adding student done");
+			try{
+				closeAll(ps, conn);
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}		
+	}
 		
-
 	// ---------------------------------- for Search ---------------------------------- //
 	public boolean doesExist(String id, Connection conn) throws SQLException {
 		//Connection conn = connection();
 		PreparedStatement ps = 
-				conn.prepareStatement("SELECT sid FROM student"
-									+ " WHERE sid = ?");
+				conn.prepareStatement("SELECT sid FROM student WHERE sid = ?");
 		ps.setString(1, id);
 		ResultSet rs = ps.executeQuery();
 
